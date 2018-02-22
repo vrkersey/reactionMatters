bddbb16aefb19a781f342afb2ada9713e47495c8
NeedHydra <doomwasd@gmail.com>
2018-02-22 16:46:11 -0700

    Add files via upload
    LevelEditor stuff

---
+++ LevelEditorInspector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelEditor))]
+public class LevelEditorInspector : Editor
+{
+   
+    public override void OnInspectorGUI()
+    {
+        LevelEditor gui = (LevelEditor)target;
+        EditorGUILayout.PrefixLabel("Level Properties");
+        gui.SizeX = EditorGUILayout.IntField("Size X", gui.SizeX);
+        gui.SizeZ = EditorGUILayout.IntField("Size Y", gui.SizeZ);
+
+        LevelEditor build = (LevelEditor)target;
+        if (GUILayout.Button("Build Grid"))
+        {
+            build.BuildGrid();
+        }
+        if (GUILayout.Button("Undo"))
+        {
+            build.Undo();
+        }
+        if (GUILayout.Button("Finish"))
+        {
+            build.Finish();
+        }
+        gui.MakeWall = EditorGUILayout.Toggle("Build Walls", gui.MakeWall);
+
+        EditorGUILayout.PrefixLabel(" ");
+        gui.ShowInputs = EditorGUILayout.Toggle("Show Default Inputs", gui.ShowInputs);
+        if(gui.ShowInputs)
+        {
+            DrawDefaultInspector();
+        }
+
+    }
+
+
+}
