4b1ed765b5cb4c9992f6b9dd4e068fd5af92054d
Victor Kersey <vrkersey@gmail.com>
2018-03-18 13:20:21 -0600

    Finished Galium wall and added in redshift of wall

--- Reaction Matters/Assets/Scripts/_wallController.cs
+++ Reaction Matters/Assets/Scripts/_wallController.cs
@@ -6,18 +6,52 @@ public class _wallController : MonoBehaviour {
 
     public bool galiumWall = true;
     public float meltTime = 5f;
+    public bool redShiftWall = true;
 
     private float heat = 0;
-    private float tempDispersion = .025f;
+    private float tempDispersion = .25f;
+    private float heating;
+    private Material material;
+    private Color startColor;
+    private Color changingColor;
 
-	// Use this for initialization
-	void Start () {
-        meltTime *= 5;
+    private float redSpeed;
+
+    private float red;
+    private float blue;
+    private float green;
+    private float alpha;
+
+    // Use this for initialization
+    void Start () {
+        meltTime *= 45;
+        heating = -.5f;
+        material = GetComponent<MeshRenderer>().material;
+        startColor = material.color;
+        changingColor = new Color();
+        changingColor.r = startColor.r;
+        changingColor.g = startColor.g;
+        changingColor.b = startColor.b;
+        changingColor.a = startColor.a;
+
+        redSpeed = redShiftWall ? (1.2f - startColor.r) / meltTime : 0;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        heat = heat - tempDispersion > 0 ? heat - tempDispersion : 0;
+	void FixedUpdate () {
+        if (Time.time - heating < .5f)
+        {
+            changingColor.r = changingColor.r + redSpeed <= 1 ? changingColor.r + redSpeed : 1;
+            heat++;
+        }
+        else
+        {
+            changingColor.r = changingColor.r >= startColor.r ? changingColor.r - (redSpeed * tempDispersion) : startColor.r;
+            heat = heat - tempDispersion > 0 ? heat - tempDispersion : 0;
+        }
+
+        material.color = changingColor;
         if (meltTime < heat)
             this.gameObject.SetActive(false);
 	}
@@ -26,7 +60,7 @@ public class _wallController : MonoBehaviour {
     {
         if (other.name == "Steam")
         {
-            heat++;
+            heating = Time.time;
         }
     }
 }
