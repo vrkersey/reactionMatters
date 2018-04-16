62785330ff87e75bcb46030dec952ea4c39bd634
Victor Kersey <vrkersey@gmail.com>
2018-04-15 19:04:06 -0600

    minor bug with save

--- Reaction Matters/Assets/Scripts/_buttonControls.cs
+++ Reaction Matters/Assets/Scripts/_buttonControls.cs
@@ -239,6 +239,10 @@ public class _buttonControls : MonoBehaviour
     }
     void OnTriggerEnter(Collider c)
     {
-        GM.Save();
+        if (c.gameObject.name == "Save Zone")
+        {
+            new WaitForSeconds(1f);
+            GM.Save();
+        }
     }
 }
