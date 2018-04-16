1f243f07de4bdeab1f20841797a498e9a9fac1b8
Victor Kersey <vrkersey@gmail.com>
2018-04-16 00:04:30 -0600

    added main menu scene, still working on it

---
+++ Reaction Matters/Assets/Scripts/_mainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _mainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("master_level", LoadSceneMode.Single);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+}
