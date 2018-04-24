ec93a60d7851d1d0d7550b73ff5b13fc8d3ca928
Matthew <matt.eggett1@gmail.com>
2018-04-23 21:49:51 -0600

    Finished level 6 recolor
    Added some of Paul's files

---
+++ Reaction Matters/Assets/Models/SwapMat/SwapMatEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SwapMAt))]
+public class SwapMatEditor : Editor {
+
+
+	// Update is called once per frame
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		SwapMAt build = (SwapMAt)target;
+		if (GUILayout.Button("Swap"))
+		{
+			build.swap();
+		}
+	}
+}
